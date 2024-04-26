5d56d9b652d0f0b747cafaafa5aa51b60bbb2cb1
Eliab_Hdez <moises_hl_zod@hotmail.com>
2024-04-25 23:26:29 -0600

    Implementada la opcion de leer el archivo "sales_file" y consultar las ventas

--- Python/ejercicio_12_Extra.py
+++ Python/ejercicio_12_Extra.py
@@ -10,7 +10,7 @@
 import os
 
 def sales_manager():
-    print('What do you want?\n 1.- Create a new sales file\n 2.- Add sale\n 3.- Exit')
+    print('What do you want?\n 1.- Create a new sales file\n 2.- Add sale\n 3.- Consult sales manager\n 4.- Exit')
     choice = input('Enter the number your choice: ')
     
     try:
@@ -28,13 +28,16 @@ def sales_manager():
         add_sale()
         
     if choice == 3:
+        consult()
+        
+    if choice == 4:
         print('Leaving the program...')
         
 def add_sale():
     product = input('Enter the product sold: ')
     quantity = int(input('Enter the quantity sold: '))
     price = float(input('Enter the price of product: $'))
-    choice = input('If a sales file does not exist, it will be created.\nDo you wish to continue? [y/n]: ').lower()
+    choice = input('If the sales file does not exist, it will be created.\nDo you wish to continue? [y/n]: ').lower()
     print('Executing...')
     if choice == 'yes' or choice == 'y':
         sales_file = open('Files/sales_file.txt', 'a')
@@ -42,5 +45,13 @@ def add_sale():
         sales_file.close()
     elif choice == 'no' or choice == 'n':
         print('Choice the option "1" to create a sales file. This option (2) is to add a new sale to the existing file')
+        
+def consult():
+    try:
+        sales_file = open('Files/sales_file.txt', 'r+')
+        print(sales_file.read())
+        sales_file.close()
+    except:
+        print('The file "sales_file" does not exist')
         
 sales_manager()
